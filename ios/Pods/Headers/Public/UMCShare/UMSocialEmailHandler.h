3e4e7ccbcb2b2b15b41fca1117849e96357cf23b
flyingish <948698823@qq.com>
2019-06-08 16:43:23 +0800

    添加友盟分享的库,没有实现分享功能(没有到新浪,腾讯开放平台创建应用)

---
+++ ios/Pods/Headers/Public/UMCShare/UMSocialEmailHandler.h
@@ -0,0 +1,1 @@
+../../../UMCShare/UMShare/SocialLibraries/Email/UMSocialEmailHandler.h
--- ios/Pods/Target Support Files/Pods-reduxNavTest/Pods-reduxNavTest-resources.sh
+++ ios/Pods/Target Support Files/Pods-reduxNavTest/Pods-reduxNavTest-resources.sh
@@ -98,9 +98,11 @@ EOM
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "${PODS_ROOT}/UMCCommonLog/UMCommonLog/UMCommonLog.bundle"
+  install_resource "${PODS_ROOT}/UMCShare/UMShare/UMSocialUI/UMSocialSDKResources.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "${PODS_ROOT}/UMCCommonLog/UMCommonLog/UMCommonLog.bundle"
+  install_resource "${PODS_ROOT}/UMCShare/UMShare/UMSocialUI/UMSocialSDKResources.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
